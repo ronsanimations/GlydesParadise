659b9d934d97539456009b742e53410624e4b23d
Ron's Animations <finkler313@gmail.com>
2026-08-26 20:57:42 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -649,7 +649,8 @@ let customPlaylistTracks = [
     "Blossom.mp3",
     "POV_ You're scrolling through the eShop.mp3",
     "Astral.mp3",
-    "Journey.mp3"
+    "Journey.mp3",
+    "Hyper.mp3"
 ]; 
 let customTrackIndex = 0;
 let customMusicActive = false;
